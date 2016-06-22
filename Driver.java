e06edd1cd852261d704662544a9220d257af4d2e
STmb2 <STmb2@STmb2s-MacBook-Air.local>
2016-06-22 19:42:08 -0400

    finished Driver

---
+++ Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+    public static void main(String[] args) {
+	Player p1 = new Strat1();
+	Player p2 = new Strat1();
+	Player r = new RandomPlayer();
+	Game game = new Game(p1, p2);
+	game.run();
+    }
+}
