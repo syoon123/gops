fce9735b54a4758052f036b56181d1bb851c63b3
STmb2 <STmb2@STmb2s-MacBook-Air.local>
2016-06-22 19:00:34 -0400

    Made makeMove Strat1

--- Strat1.java
+++ Strat1.java
@@ -1,9 +1,10 @@
-//test strategy
+//test strategy: matches bid
 public class Strat1 extends Player {
     public Strat1() {
 	super();
     }
-    public int makeMove() {
-	return 0;
+    public int makeMove(){
+	_hand.remove(_hand.indexOf(_prize));
+	return _prize;
     }
 }
