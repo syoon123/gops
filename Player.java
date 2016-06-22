33612a307d5ee1e807813e37f0e4a29d143bc8f0
SAmb2 <SAmb2@SAmb2s-MacBook-Air.local>
2016-06-22 18:15:28 -0400

    Finish superclass Player

--- Player.java
+++ Player.java
@@ -1,2 +1,26 @@
-public class Player {
+public abstract class Player {
+
+    //inst vars
+    ArrayList<Integer> _hand;
+    int _score;
+
+    //methods
+    public String toString() {
+	String retStr;
+	for (Integer i : _hand) {
+	    retStr = i + " ";
+	}
+	return retStr;
+    }
+
+    public int getScore() {
+	return _score;
+    }
+
+    public ArrayList<Integer> getHand() {
+	return _hand;
+    }
+
+    public abstract int makeMove();
+    
 }
