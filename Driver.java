68eacb995542af1c69a55e720669252cf1c270b9
STmb2 <STmb2@STmb2s-MacBook-Air.local>
2016-06-22 20:11:11 -0400

    Changed to Strat2

--- Driver.java
+++ Driver.java
@@ -1,7 +1,7 @@
 public class Driver {
     public static void main(String[] args) {
 	Player p1 = new Strat1();
-	Player p2 = new Strat1();
+	Player p2 = new Strat2();
 	Player r = new RandomPlayer();
 	Game game = new Game(p1, p2);
 	game.run();
