683f1fc0629c5b1730541a29ae663af49959f915
STmb2 <STmb2@STmb2s-MacBook-Air.local>
2016-06-25 13:37:16 -0400

    runs game multiple times

--- Driver.java
+++ Driver.java
@@ -1,8 +1,11 @@
+import cs1.Keyboard;
 public class Driver {
     public static void main(String[] args) {
 	Player p1 = new Strat1();
 	Player p2 = new Strat2();
 	Game game = new Game(p1, p2);
-	game.run();
+	System.out.println("How many times do you want the game to run?");
+	int n = Keyboard.readInt();
+	for (int i = 0; i < n; i++) game.run();
     }
 }
