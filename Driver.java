135efc51a1bbf706f14740a7550368425f8b5c8b
SAmb2 <SAmb2@SAmb2s-MacBook-Air.local>
2016-06-25 14:53:21 -0400

    Fix bug

--- Driver.java
+++ Driver.java
@@ -7,16 +7,18 @@ public class Driver {
 	System.out.println("Please type in the integer corresponding to the strategy for Player 2.");
 	System.out.println("1. User" + "\n" + "2. x" + "\n"  + "3. x+1");
 	int strat2 = Keyboard.readInt();
-	Player p1, p2;
-	if (strat1 == 1)  p1 = new User();
-	else if (strat1 == 2) p1 = new Strat1();
-	else p1 = new Strat2();
-	if (strat2 == 1)  p2 = new User();
-	else if (strat2 == 2) p2 = new Strat1();
-	else p2 = new Strat2();
-	Game game = new Game(p1, p2);
 	System.out.println("How many times do you want the game to run?");
 	int n = Keyboard.readInt();
-	for (int i = 0; i < n; i++) game.run();
+	for (int i = 0; i < n; i++) {	    
+	    Player p1, p2;
+	    if (strat1 == 1)  p1 = new User();
+	    else if (strat1 == 2) p1 = new Strat1();
+	    else p1 = new Strat2();
+	    if (strat2 == 1)  p2 = new User();
+	    else if (strat2 == 2) p2 = new Strat1();
+	    else p2 = new Strat2();
+	    Game game = new Game(p1, p2);
+	    game.run();
+	}
     }
 }
