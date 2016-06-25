04d6ac78b8a7d4a212a80843dd687f750a41531d
STmb2 <STmb2@STmb2s-MacBook-Air.local>
2016-06-25 14:28:29 -0400

    combined with ui

--- Game.java
+++ Game.java
@@ -22,6 +22,14 @@ public class Game {
     }
     public void turn() {
 	setPrize();
+	if (_p1 instanceof User) {
+	    System.out.println();
+	    System.out.println("Player 2 has these cards left:");
+	    System.out.println(_p2);
+	    System.out.println("The random player has these cards left:");
+	    System.out.println(_r);
+	    System.out.println();
+	}
 	int move1 = _p1.makeMove();
 	int move2 = _p2.makeMove();
 	int move3 = _r.makeMove();
@@ -34,6 +42,15 @@ public class Game {
 	else if (move3 > move1 && move3 > move2) {
 	    _r.setScore(_prize);
 	}
+	if (_p1 instanceof User) {
+	    System.out.println
+		("You put down a " + move1 + ".");
+	    System.out.println
+		("Player 2 put down a " + move2 + ".");
+	    System.out.println
+		("The random player put down a " + move3 + ".");
+	    System.out.println();
+	}
     }
     public void run() {
 	while (_deck.size() > 0) {
@@ -42,8 +59,12 @@ public class Game {
 	results();
     }
     public void results() {
-	System.out.println(_p1.getScore());
-	System.out.println(_p2.getScore());
-	System.out.println(_r.getScore());
+	System.out.println
+	    ("Your score is " + _p1.getScore() + ".");
+	System.out.println
+	    ("Player 2's score is " + _p2.getScore() + ".");
+	System.out.println
+	    ("The random player's score is " + _r.getScore() + ".");
+	System.out.println(); 
     }
 }
