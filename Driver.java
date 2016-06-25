02f13137dbe5592c208f4c3a0f3c5495442b1c88
STmb2 <STmb2@STmb2s-MacBook-Air.local>
2016-06-25 14:35:16 -0400

    combined with ui

--- Driver.java
+++ Driver.java
@@ -1,8 +1,18 @@
 import cs1.Keyboard;
 public class Driver {
     public static void main(String[] args) {
-	Player p1 = new Strat1();
-	Player p2 = new Strat2();
+	System.out.println("Please type in the integer corresponding to the strategy for Player 1.");
+	System.out.println("1. User" + "\n" + "2. x" + "3. x+1");
+	int strat1 = Keyboard.readInt();
+	System.out.println("Please type in the integer corresponding to the strategy for Player 2.");
+	System.out.println("1. User" + "\n" + "2. x" + "3. x+1");
+	int strat2 = Keyboard.readInt();
+	if (strat1 == 1) Player p1 = new User();
+	else if (strat1 == 2) p1 = new Strat1();
+	else if (strat1 == 3) p1 = new Strat2();
+	if (strat2 == 1) Player p1 = new User();
+	else if (strat2 == 2) p1 = new Strat1();
+	else if (strat2 == 3) p1 = new Strat2();
 	Game game = new Game(p1, p2);
 	System.out.println("How many times do you want the game to run?");
 	int n = Keyboard.readInt();
