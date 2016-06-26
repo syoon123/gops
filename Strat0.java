f2f49a10b578d8c64a9c50fd0cb85f0d8c7823d8
SAmb2 <SAmb2@SAmb2s-MacBook-Air.local>
2016-06-26 14:39:46 -0400

    Change Strat names

---
+++ Strat0.java
@@ -0,0 +1,9 @@
+//test strategy: matches bid
+public class Strat1 extends Player {
+    public Strat1() {
+	super();
+    }
+    public int makeMove(){
+	return _hand.remove(_hand.indexOf(_prize));
+    }
+}
--- Strat1.java
+++ Strat1.java
@@ -1,9 +1,9 @@
-//test strategy: matches bid
-public class Strat1 extends Player {
-    public Strat1() {
+//test strategy: matches bid + 1, or mod
+public class Strat2 extends Player {
+    public Strat2() {
 	super();
     }
     public int makeMove(){
-	return _hand.remove(_hand.indexOf(_prize));
+	return (_hand.remove(_hand.indexOf(_prize))+1))%14;
     }
 }
