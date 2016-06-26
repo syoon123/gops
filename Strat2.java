33e5ab42034e94e12f28d7e52cf2674721663ddf
STmb2 <STmb2@STmb2s-MacBook-Air.local>
2016-06-26 14:14:12 -0400

    added mod

--- Strat2.java
+++ Strat2.java
@@ -1,9 +1,9 @@
-//test strategy: matches bid + 1
+//test strategy: matches bid + 1, or mod
 public class Strat2 extends Player {
     public Strat2() {
 	super();
     }
     public int makeMove(){
-	return _hand.remove(_hand.indexOf(_prize))+1;
+	return (_hand.remove(_hand.indexOf(_prize))+1))%14;
     }
 }
